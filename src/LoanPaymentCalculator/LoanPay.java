e6a233baa9ae197b422440d49025fe96f4342666
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-24 15:08:31 -0600

    Add initial variables

--- src/LoanPaymentCalculator/LoanPay.java
+++ src/LoanPaymentCalculator/LoanPay.java
@@ -1,7 +1,10 @@
 package LoanPaymentCalculator;
 
 public class LoanPay {
-
+    int carLoan = 10000;
+    int loanLength = 3;
+    int interestRate = 5;
+    int downPayment = 2000;
 
     public static void main(String[] args) {
 
