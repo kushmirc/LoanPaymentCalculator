792687c0d20b047cc0312e98e32c5c3c0f169176
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-24 15:21:37 -0600

    Add conditional to ensure positive loanLength and interestRate

--- src/LoanPaymentCalculator/LoanPay.java
+++ src/LoanPaymentCalculator/LoanPay.java
@@ -1,12 +1,17 @@
 package LoanPaymentCalculator;
 
 public class LoanPay {
-    int carLoan = 10000;
-    int loanLength = 3;
-    int interestRate = 5;
-    int downPayment = 2000;
+
 
     public static void main(String[] args) {
+        int carLoan = 10000;
+        int loanLength = 3;
+        int interestRate = 5;
+        int downPayment = 2000;
 
+        if (loanLength <= 0 || interestRate <= 0){
+            System.out.println("Error! You must take out a valid car loan.");
+        }
     }
+
 }
